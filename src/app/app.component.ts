62d0be267c4c72e8830faa345787ad2883b3738a
Faran Javed <faran1997@outlook.com>
2023-09-14 16:57:49 +0500

    property binding

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
+// App starts here
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HomeComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+
+// this is home component
+export class AppComponent {
+  title = 'homes';
+}
