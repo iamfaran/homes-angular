930604b635e83fbfc044700c74f943c560f64713
Faran Javed <faran1997@outlook.com>
2023-09-18 12:53:40 +0500

    basic router testing

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,17 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      details works!
-    </p>
-  `,
-  styleUrls: ['./details.component.css']
+  template: ` <p>details works!</p> `,
+  styleUrls: ['./details.component.css'],
 })
-export class DetailsComponent {
-
-}
+export class DetailsComponent {}
